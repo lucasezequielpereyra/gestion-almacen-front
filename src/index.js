df7148d40a39b65622a2ec75e33a33c95f34ed39
Lucas Pereyra <pereyral@jefatura.gob.ar>
2023-02-21 13:45:09 -0300

    update index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { store } from './common/redux/store'
+import { Provider } from 'react-redux'
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+ReactDOM.render(<App />, document.getElementById('root'))
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+)
