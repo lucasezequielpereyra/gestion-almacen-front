af6acce52bf4bf4cfce58d89205a687ea448855f
Lucas Pereyra <pereyral@jefatura.gob.ar>
2023-02-21 22:35:57 -0300

    add routing in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import React from "react";
+import { store } from './common/redux/store'
+import { Provider } from 'react-redux'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Layout from './common/containers/layout'
+import Public from './common/views/public'
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>hello</h1>
-    </div>
-  );
-};
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            {/* public routes */}
+            <Route path="/login" element={<Public />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  )
+}
 
-export default App;
+export default App
