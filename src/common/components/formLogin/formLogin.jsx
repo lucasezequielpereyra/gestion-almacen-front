43dac485f6129700617514189151ab13eec71b87
Lucas Pereyra <pereyral@jefatura.gob.ar>
2023-02-21 22:43:46 -0300

    add functional login

--- src/common/components/formLogin/formLogin.jsx
+++ src/common/components/formLogin/formLogin.jsx
@@ -1,23 +1,51 @@
 import React from 'react'
 import styles from './formLogin.module.scss'
 
-const FormLogin = () => {
+const FormLogin = ({
+  userRef,
+  errRef,
+  errMsg,
+  user,
+  passowrd,
+  handleSubmit,
+  handleUserInput,
+  handlePwdInput
+}) => {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Ingrese al sistema</h1>
       <div className={styles.login}>
         <div className={styles.form}>
           <div className={styles.input}>
-            <input type="text" placeholder="Username" />
+            <input
+              type="text"
+              placeholder="Username"
+              ref={userRef}
+              id="username"
+              onChange={handleUserInput}
+              autoComplete="off"
+              value={user}
+              required
+            />
           </div>
           <div className={styles.input}>
-            <input type="password" placeholder="Password" />
+            <input
+              type="password"
+              placeholder="Password"
+              id="password"
+              onChange={handlePwdInput}
+              value={passowrd}
+              required
+            />
           </div>
           <div className={styles.button}>
-            <button>Ingresar</button>
+            <button onClick={handleSubmit}>Ingresar</button>
           </div>
         </div>
       </div>
+      <p className={styles.errMsg} ref={errRef}>
+        {errMsg}
+      </p>
     </div>
   )
 }
--- src/common/views/public/public.jsx
+++ src/common/views/public/public.jsx
@@ -1,10 +1,82 @@
-import { useEffect } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import FormLogin from '../../components/formLogin'
 import { selectCurrentToken } from '../../redux/auth/authSlice'
-import { Navigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { login } from '../../redux/auth/authSlice'
+import { useLoginMutation } from '../../redux/auth/authApiSlice'
+import { useNavigate } from 'react-router-dom'
 
 const Public = () => {
-  return <FormLogin />
+  const token = useSelector(selectCurrentToken)
+  const navigate = useNavigate()
+
+  // for redux
+  const dispatch = useDispatch()
+  const [auth, { isLoading }] = useLoginMutation()
+
+  // for form
+  const userRef = useRef()
+  const errRef = useRef()
+  const [userbame, setUser] = useState('')
+  const [err, setErr] = useState('')
+  const [passowrd, setPassword] = useState('')
+
+  useEffect(() => {
+    if (token) {
+      return navigate('/dashboard')
+    }
+  }, [token])
+
+  useEffect(() => {
+    userRef.current.focus()
+  }, [])
+
+  useEffect(() => {
+    setErr('')
+  }, [userbame, passowrd])
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+    try {
+      const userData = await auth({
+        username: userbame,
+        password: passowrd
+      }).unwrap()
+      console.log(userData)
+
+      dispatch(login(...userData, userbame))
+      setUser('')
+      setPassword('')
+      navigate('/dashboard')
+    } catch (err) {
+      if (!err.status) {
+        setErr('No Server Response !')
+      } else if (err.status === 400) {
+        setErr('Missing Username or Password !')
+      } else if (err.status === 401) {
+        setErr('Unauthorized !')
+      } else {
+        setErr('Login Failed !')
+      }
+      userRef.current.focus()
+    }
+  }
+
+  const handleUserInput = e => setUser(e.target.value)
+  const handlePwdInput = e => setPassword(e.target.value)
+
+  return (
+    <FormLogin
+      userRef={userRef}
+      errRef={errRef}
+      errMsg={err}
+      user={userbame}
+      passowrd={passowrd}
+      handleSubmit={handleSubmit}
+      handleUserInput={handleUserInput}
+      handlePwdInput={handlePwdInput}
+    />
+  )
 }
 
 export default Public
