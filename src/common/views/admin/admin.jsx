61c51d63f227117da1dbae20c9c80c45a31ecb34
Lucas Pereyra <lucasezequielpereyra0@gmail.com>
2023-03-09 18:33:20 -0300

    delete subLayout

--- src/common/views/admin/admin.jsx
+++ src/common/views/admin/admin.jsx
@@ -1,13 +1,10 @@
 import HeaderView from '../header/header'
-import SubLayout from '../../containers/subLayout'
 
 const Admin = () => {
   return (
     <div>
       <HeaderView />
-      <SubLayout>
-        <h1>Admin</h1>
-      </SubLayout>
+      <h1>Admin</h1>
     </div>
   )
 }
