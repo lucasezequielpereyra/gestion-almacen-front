c720ce310d3c4f65d7d506e18e3bea1ae3342945
Lucas Pereyra <lucasezequielpereyra0@gmail.com>
2023-07-25 22:41:12 -0300

    fix - add product redux bug

--- src/common/hooks/useUpdateProducts.js
+++ src/common/hooks/useUpdateProducts.js
@@ -1,13 +1,15 @@
 import { useEffect } from 'react'
 import { useGetProductsQuery } from '../redux/products/productsApiSlice'
-import { getProducts } from '../redux/products/productsSlice'
-import { useDispatch } from 'react-redux'
+import { getProducts, selectCurrentProducts } from '../redux/products/productsSlice'
+import { useDispatch, useSelector } from 'react-redux'
 
 export const useUpdateProducts = () => {
+  const products = useSelector(selectCurrentProducts)
   const dispatch = useDispatch()
   const { data: data, isSuccess } = useGetProductsQuery()
+
   useEffect(() => {
-    if (isSuccess) {
+    if (isSuccess && products.length === 0) {
       dispatch(getProducts(data))
     }
   }, [data])
