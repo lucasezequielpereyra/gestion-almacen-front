364b199226d29c75ff7ad32ff44ee930324e6c12
Lucas Pereyra <lucasezequielpereyra0@gmail.com>
2023-08-02 11:35:13 -0300

    feat - add delete product button in productsTable component

--- src/common/components/productsTable/productsTable.jsx
+++ src/common/components/productsTable/productsTable.jsx
@@ -2,19 +2,24 @@ import propTypes from 'prop-types'
 import styles from './productsTable.module.scss'
 import Form from '../form/'
 import AddProductItems from '../../../views/admin/products/components/formItems/addProductItems'
+import Spinner from '../spinner/spinner'
 
 const ProductsTable = ({
   products,
-  modal,
+  modalEdit,
   formValues,
-  handleModal,
   handleChange,
   handleSubmit,
   msgError,
-  categories
+  categories,
+  handleModalEdit,
+  handleModalDelete,
+  handleDelete,
+  modalDelete,
+  loading
 }) => {
   return (
-    <>
+    <div className={styles.container}>
       <div className={styles.table}>
         <table>
           <thead>
@@ -33,8 +38,11 @@ const ProductsTable = ({
               <tr key={index}>
                 <td className={styles.sku}>
                   {product.sku}
-                  <button className={styles.buttonHover} onClick={() => handleModal(product)}>
+                  <button className={styles.editBtn} onClick={() => handleModalEdit(product)}>
                     editar
+                  </button>
+                  <button className={styles.deleteBtn} onClick={() => handleModalDelete(product)}>
+                    X
                   </button>
                 </td>
                 <td>{product.name}</td>
@@ -47,24 +55,40 @@ const ProductsTable = ({
             ))}
           </tbody>
         </table>
-        {modal && (
-          <Form
-            handleModal={handleModal}
-            active={modal}
-            handleSubmit={handleSubmit}
-            msgError={msgError}
-            modalTitle="Editar Producto"
-          >
-            <AddProductItems
-              formValues={formValues}
-              handleChange={handleChange}
-              categories={categories}
-              buttonLabel="Actualizar"
-            />
-          </Form>
-        )}
       </div>
-    </>
+      {modalEdit && (
+        <Form
+          handleModal={handleModalEdit}
+          active={modalEdit}
+          handleSubmit={handleSubmit}
+          msgError={msgError}
+          modalTitle="Editar Producto"
+        >
+          <AddProductItems
+            formValues={formValues}
+            handleChange={handleChange}
+            categories={categories}
+            buttonLabel="Actualizar"
+          />
+        </Form>
+      )}
+      {modalDelete && (
+        <Form
+          handleModal={handleModalDelete}
+          active={modalDelete}
+          handleSubmit={handleDelete}
+          msgError={msgError}
+          modalTitle="Eliminar Producto"
+        >
+          <div className={styles.formDelete}>
+            <p>
+              ¿Estas seguro que deseas eliminar el producto <strong>{formValues.name}</strong>?
+            </p>
+            <button type="submit">{loading && <Spinner size="sm" />} eliminar</button>
+          </div>
+        </Form>
+      )}
+    </div>
   )
 }
 
@@ -72,11 +96,16 @@ export default ProductsTable
 
 ProductsTable.propTypes = {
   products: propTypes.array.isRequired,
-  modal: propTypes.bool.isRequired,
-  handleModal: propTypes.func.isRequired,
+  modalEdit: propTypes.bool.isRequired,
+  handleDelete: propTypes.func.isRequired,
   formValues: propTypes.object.isRequired,
   handleChange: propTypes.func.isRequired,
   handleSubmit: propTypes.func.isRequired,
   msgError: propTypes.string.isRequired,
-  categories: propTypes.array.isRequired
+  categories: propTypes.array.isRequired,
+  handleModalEdit: propTypes.func.isRequired,
+  handleModalDelete: propTypes.func.isRequired,
+  handleDelete: propTypes.func.isRequired,
+  modalDelete: propTypes.bool.isRequired,
+  loading: propTypes.bool.isRequired
 }
