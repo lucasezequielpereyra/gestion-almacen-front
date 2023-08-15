ce804cf7e9859b1b64542dcc595b65b83efc44fd
Lucas Pereyra <lucasezequielpereyra0@gmail.com>
2023-08-15 17:06:35 -0300

    feat - add product for sales in dashboard

--- src/views/dashboard/components/content/content.jsx
+++ src/views/dashboard/components/content/content.jsx
@@ -1,30 +1,71 @@
-import { useState, useEffect } from 'react'
-import { useSelector } from 'react-redux'
-import { selectCurrentProducts } from '../../../../common/redux/products/productsSlice'
-import { FaCheck } from 'react-icons/fa6'
+import { FaCheck, FaCircleExclamation } from 'react-icons/fa6'
 import styles from './content.module.scss'
 import propTypes from 'prop-types'
 
-const Content = ({ inputRef }) => {
-  const reduxProds = useSelector(selectCurrentProducts)
-  const [products, setProducts] = useState([])
-
-  useEffect(() => {
-    setProducts(reduxProds)
-  }, [reduxProds])
-
+const Content = ({
+  inputSearchRef,
+  quantityRef,
+  handleAddProduct,
+  handleDeleteProduct,
+  msg,
+  cartProducts,
+  products
+}) => {
   return (
     <div className={styles.container}>
       <div className={styles.searchProduct}>
-        <input list="products" name="products" placeholder="Busca un producto..." ref={inputRef} />
+        <input
+          list="products"
+          name="products"
+          placeholder="Busca un producto..."
+          ref={inputSearchRef}
+        />
+        <input type="number" name="quantity" defaultValue={1} ref={quantityRef} />
         <datalist id="products">
           {products.map((product, index) => (
             <option key={index} value={product.name} />
           ))}
         </datalist>
-        <button>
+        <button onClick={handleAddProduct}>
           <FaCheck className={styles.icon} />
         </button>
+      </div>
+      <div className={styles.msgSearch}>
+        {msg && (
+          <p>
+            <FaCircleExclamation /> {msg}
+          </p>
+        )}
+      </div>
+      <div className={styles.table}>
+        <table>
+          <thead>
+            <tr>
+              <th>producto</th>
+              <th>cantidad</th>
+              <th>precio</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cartProducts.map((product, index) => {
+              return (
+                <tr key={index}>
+                  <td>
+                    {product.name}{' '}
+                    <button
+                      className={styles.deleteProduct}
+                      onClick={handleDeleteProduct(product.name)}
+                    >
+                      quitar
+                    </button>
+                  </td>
+                  <td>{product.quantity}</td>
+                  <td>{product.price}</td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
       </div>
     </div>
   )
@@ -33,5 +74,11 @@ const Content = ({ inputRef }) => {
 export default Content
 
 Content.propTypes = {
-  inputRef: propTypes.object.isRequired
+  inputSearchRef: propTypes.object.isRequired,
+  quantityRef: propTypes.object.isRequired,
+  handleAddProduct: propTypes.func.isRequired,
+  handleDeleteProduct: propTypes.func.isRequired,
+  msg: propTypes.string.isRequired,
+  cartProducts: propTypes.array.isRequired,
+  products: propTypes.array.isRequired
 }
--- src/views/dashboard/dashboard.jsx
+++ src/views/dashboard/dashboard.jsx
@@ -1,27 +1,98 @@
 import Header from './components/header'
 import Content from './components/content/'
-import { useRef, useEffect } from 'react'
+import { useRef, useEffect, useState } from 'react'
 import { useUpdateProducts, useUpdateInactiveProducts } from '../../common/hooks/useUpdateProducts'
 import { useUpdateCategories } from '../../common/hooks/useUpdateCategories'
+import { useSelector } from 'react-redux'
+import { selectCurrentProducts } from '../../common/redux/products/productsSlice'
 
 const Dashboard = () => {
   useUpdateProducts()
   useUpdateInactiveProducts()
   useUpdateCategories()
 
-  // input search ref
-  const inputSearchRef = useRef(null)
+  const reduxProds = useSelector(selectCurrentProducts)
 
+  // state hooks
+  const [products, setProducts] = useState([])
+  const [cartProducts, setCartProducts] = useState([])
+  const [msg, setMsg] = useState('')
+
+  // refs hooks
+  const inputSearchRef = useRef()
+  const quantityRef = useRef()
+
+  // effect hooks
   useEffect(() => {
     inputSearchRef.current.focus()
-  }, [])
+  }, [inputSearchRef])
 
-  useEffect(() => {}, [])
+  useEffect(() => {
+    setProducts(reduxProds)
+  }, [reduxProds])
+
+  // handlers
+  const handleAddProduct = () => {
+    setMsg('')
+
+    const product = reduxProds.find(prod => prod.name === inputSearchRef.current.value)
+
+    if (!product) return setMsg('El producto no existe')
+    if (quantityRef.current.value < 1) return setMsg('La cantidad debe ser mayor a 0')
+
+    if (cartProducts.length === 0) {
+      const newProduct = {
+        name: product.name,
+        quantity: quantityRef.current.value,
+        price: product.price_sale
+      }
+      inputSearchRef.current.value = ''
+      quantityRef.current.value = 1
+      return setCartProducts([newProduct])
+    }
+
+    if (cartProducts.find(prod => prod.name === product.name)) {
+      const newCartProducts = cartProducts.map(prod => {
+        if (prod.name === product.name) {
+          prod.quantity = parseInt(prod.quantity) + parseInt(quantityRef.current.value)
+        }
+        return prod
+      })
+      inputSearchRef.current.value = ''
+      quantityRef.current.value = 1
+      return setCartProducts(newCartProducts)
+    }
+
+    const newProduct = {
+      name: product.name,
+      quantity: quantityRef.current.value,
+      price: product.price_sale
+    }
+    inputSearchRef.current.value = ''
+    quantityRef.current.value = 1
+
+    setCartProducts([...cartProducts, newProduct])
+  }
+
+  const handleDeleteProduct = product => {
+    return () => {
+      let newCartProducts = cartProducts.filter(prod => prod.name !== product)
+      setCartProducts(newCartProducts)
+    }
+  }
 
   return (
     <div>
       <Header />
-      <Content inputRef={inputSearchRef} />
+      <Content
+        inputSearchRef={inputSearchRef}
+        quantityRef={quantityRef}
+        handleAddProduct={handleAddProduct}
+        handleDeleteProduct={handleDeleteProduct}
+        msg={msg}
+        cartProducts={cartProducts}
+        products={products}
+      />
     </div>
   )
 }
