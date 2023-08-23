cad21935e5f831660f8cad06574bf323e5c3ad1a
Lucas Pereyra <lucasezequielpereyra0@gmail.com>
2023-08-23 03:21:49 -0300

    feat - add hok for update employees

---
+++ src/common/hooks/useUpdateEmployees.js
@@ -0,0 +1,16 @@
+import { useEffect } from 'react'
+import { useGetEmployeesQuery } from '../redux/employees/employeesApiSlice'
+import { getEmployees, selectCurrentEmployees } from '../redux/employees/employeesSlice'
+import { useDispatch, useSelector } from 'react-redux'
+
+export const useUpdateEmployees = () => {
+  const employees = useSelector(selectCurrentEmployees)
+  const dispatch = useDispatch()
+  const { data: data, isSuccess } = useGetEmployeesQuery()
+
+  useEffect(() => {
+    if (isSuccess && employees.length === 0) {
+      dispatch(getEmployees(data))
+    }
+  }, [data])
+}
